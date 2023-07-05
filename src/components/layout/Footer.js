db8b64f8eb4a6452416351335d3be2271d05c735
GabrielAgbede <115146706+GabrielAgbede@users.noreply.github.com>
2023-07-05 05:48:25 -0400

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -16,8 +16,8 @@ import logoWhiteImage from './assets/logo-white.png';
 import pages from '../../utils/pages';
 
 const contacts = [
-  { icon: faLocationDot, info: '678 Pisa Ave, Chicago, IL 60611', },
-  { icon: faPhone, info: '(312) 593-2744', },
+  { icon: faLocationDot, info: '28 eletu odibo st, yaba, lagos', },
+  { icon: faPhone, info: '2348159568887', },
   { icon: faEnvelope, info: 'customer@littlelemon.com', },
 ];
 
